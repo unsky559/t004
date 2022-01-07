f6db6e9fa9368ab1f7accb124f0b3621ac644283
unsky559 <6kostya@gmail.com>
2022-01-07 14:59:55 +0200

    feat: dropdowns

---
+++ src/components/dropdownControl/dropdownControl.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./dropdownLangSwitch.scss";
+
+const DropdownLangSwitch = () => {
+    return (
+        <div className="dropdownLangSwitch">
+            <ul>
+                <li className="selected"><a href="#">Herbrew</a></li>
+                <li><a href="#">asd</a></li>
+                <li><a href="#">asdasd</a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default DropdownLangSwitch;
